25e10e163e840cbc3eca14254676da34e251c7d7
Dennis.Winter <dennis.winter@rossmann.de>
2018-12-06 16:59:34 +0100

    spring initializr init

---
+++ src/main/java/de/nuro/rawlearning/RawlearningApplication.java
@@ -0,0 +1,12 @@
+package de.nuro.rawlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawlearningApplication.class, args);
+	}
+}
